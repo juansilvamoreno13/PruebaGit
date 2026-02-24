3af8d8f7ea13ceabc3316b6cb9c2611c76713f1a
Juan Silva Moreno <juansilvania13@gmail.com>
2026-02-24 18:02:14 +0100

    Creamos la clase alumno

---
+++ src/modelo/Alumno.java
@@ -0,0 +1,14 @@
+package modelo;
+
+public class Alumno {
+
+public Alumno() {
+
+String alumno = "Juan";
+int edad = 19;
+
+System.out.println("El alumno se llama " + alumno + " y tiene " + edad + " años");
+
+}
+
+}
---
+++ src/vista/main.java
@@ -0,0 +1,13 @@
+package vista;
+
+import modelo.Alumno;
+
+public class main {
+
+public static void main(String[] args) {
+    
+Alumno Alumno = new Alumno();
+
+}
+
+}
